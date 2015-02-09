17e89728815e7e15cde42ba5707fb8e3e396a60d
Kane Stapler <kanestapler@gmail.com>
2015-02-09 11:03:30 -0600

    Ricky 2/8

--- src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -3,6 +3,17 @@ package edu.jsu.mcis;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class VolumeCalculatorTest {
+public class ArgumentParserTest {
+    @Test
+    public void testAddArgumentLengthToAP() {
+        ArgumentParser ap = new ArgumentParser();
+        ap.addArg("Length");
+        assertEquals("Length", ap.getArg(0));
+    }
     
+    @Test
+    public void testFourPositionalArgumentsIsError() {
+        ArgumentParser ap = new ArgumentParser();
+        //ap.parse(new String[] {"4", "2", "1", "42"});
+    }
 }
