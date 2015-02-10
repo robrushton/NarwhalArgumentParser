8c94934674fa681b8711ace476c10e9fdbbf14a8
Kane Stapler <kanestapler@gmail.com>
2015-02-10 14:04:46 -0600

    Rick

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -3,17 +3,7 @@ package edu.jsu.mcis;
 import java.util.ArrayList;
 
 public class ArgumentParser {
-    private ArrayList<String> arguments = new ArrayList<String>();
-    public void addArg(String arg) {
-        arguments.add(arg);
-    }
     
-    public String getArg(int n) {
-        return arguments.get(n);
-    }
-public boolean addArg(){
-		return false;
-	}
 		
 
 }
--- src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -4,31 +4,12 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ArgumentParserTest {
-    @Test
-    public void testAddArgumentLengthToAP() {
-        ArgumentParser ap = new ArgumentParser();
-        ap.addArg("Length");
-        assertEquals("Length", ap.getArg(0));
-    }
     
-    @Test
-    public void testFourPositionalArgumentsIsError() {
+	@Test
+    testGetArgWithLengthOf10() {
         ArgumentParser ap = new ArgumentParser();
-        //ap.parse(new String[] {"4", "2", "1", "42"});
+        myArgs.put("Length", 10);
+        assertEquals(ap.getArg("Length"), 10);
     }
-
-	@Test 
-	public void testEmptyExit(){
-		ArgumentParser ap = new ArgumentParser();
-		boolean b = ap.addArg();
-		assertFalse(b);
-	}
-	
-	//@Test 
-	public void testPositionalArguments(){
-		ArgumentParser ap = new ArgumentParser();
-	
-	}
-	
 
 }
