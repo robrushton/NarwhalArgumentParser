ad56dee0a161f472a74ddb6b53ba884d64dd98b6
Kane Stapler <kanestapler@gmail.com>
2015-02-10 14:25:47 -0600

    Rick

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -1,15 +1,6 @@
 package edu.jsu.mcis;
 
 import java.util.*;
-<<<<<<< HEAD
-
-public class ArgumentParser {
-    
-    
-	
-
-}
-=======
 
 
 public class ArgumentParser {
@@ -17,6 +8,5 @@ public class ArgumentParser {
     public static Map<String, Integer> myArgs = new HashMap<>();
     public static LinkedList myNames = new LinkedList();
         
-    }
-    
->>>>>>> 8c05f9d6f062c3c137e11652790b1aa59ae2b0df
+}
+
