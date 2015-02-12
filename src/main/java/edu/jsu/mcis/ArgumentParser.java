7e0b8e90effe5250c0e782f9b6f22894fc9c6346
dpctrey <dchristian@jsu.edu>
2015-02-11 20:54:28 -0600

    Dwight 2/11 @ 8:54

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -3,21 +3,72 @@ package edu.jsu.mcis;
 import java.util.*;
 
 
-public class ArgumentParser extends ArgumentObject{
-    
-    ArgumentObject test = new ArgumentObject();
-    
-    private static Map<String, Integer> myArgs = new HashMap<>();
-    private static Map<String, ArgumentObject> myArgs2 = new HashMap<String, ArgumentObject>();
-    private static LinkedList myNames = new LinkedList();
-    
-    public void addArg(String s, int i) {
-        myArgs.put(s, i);
-    }
-    
-    public int getValue(String s) {
-        return myArgs.get(s);
-    }
+public class ArgumentParser
+{
         
-}
+    private static final Map<String, ArgumentObject> myArgs = new HashMap<>();
+    private static final LinkedList myNames = new LinkedList();
+    
+    static class ArgumentObject 
+    {
 
+        String myDescription = null;
+        int myInt;
+        float myFloat;
+        String myString = null;
+        boolean myBool;
+
+        public String getDescriptionValue() 
+        {
+            return myDescription;
+        }
+
+        public void setDescriptionValue(String d) 
+		{
+            myDescription = d;
+        }
+
+        public int getIntValue() 
+		{
+            return myInt;
+        }
+
+        public void setIntValue(int i) 
+		{
+            myInt = i;
+        }
+
+        public float getFloatValue() 
+		{
+            return myFloat;
+        }
+
+        public void setFloatValue(float f) 
+		{
+            myFloat = f;
+        }
+
+        public String getStringValue() 
+		{
+            return myString;
+        }
+
+        public void setStringValue(String s) 
+		{
+            myString = s;
+        }
+
+        public boolean getBooleanValue() 
+		{
+            return myBool;
+        }
+
+        public void setBooleanValue(boolean b) 
+		{
+            myBool = b;
+        }
+    }
+	public void addArg(String s, ArgumentObject a){
+		myArgs.put(s, a);
+	}
+}
