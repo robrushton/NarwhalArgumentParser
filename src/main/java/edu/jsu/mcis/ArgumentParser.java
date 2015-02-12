412dddb6fa1bef46ced795cf92514d13b83212dc
Kane Stapler <kanestapler@gmail.com>
2015-02-11 18:23:51 -0600

    Kane adding Object

--- src/main/java/edu/jsu/mcis/ArgumentParser.java
+++ src/main/java/edu/jsu/mcis/ArgumentParser.java
@@ -8,7 +8,7 @@ public class ArgumentParser extends ArgumentObject{
     ArgumentObject test = new ArgumentObject();
     
     private static Map<String, Integer> myArgs = new HashMap<>();
-    //private static Map<String, ArgumentObject> myArgs2 = new HashMap<String, ArgumentObject>();
+    private static Map<String, ArgumentObject> myArgs2 = new HashMap<String, ArgumentObject>();
     private static LinkedList myNames = new LinkedList();
     
     public void addArg(String s, int i) {
