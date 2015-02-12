0994dc955ff71ff8fbd4c28aeef4afd534c4161c
dpctrey <dchristian@jsu.edu>
2015-02-11 20:55:41 -0600

    Dwight 2/11 @ 8:55

--- src/test/java/edu/jsu/mcis/ArgumentParserTest.java
+++ src/test/java/edu/jsu/mcis/ArgumentParserTest.java
@@ -6,27 +6,29 @@ import static org.junit.Assert.*;
 public class ArgumentParserTest {
     
     @Test
-    public void testGetArgWithLengthOf10() {
+    public void testObjectIntValue() 
+	{
         ArgumentParser ap = new ArgumentParser();
-        ap.addArg("Length", 10);
-        int t = ap.getValue("Length");
+		ArgumentParser.ArgumentObject ao = new ArgumentParser.ArgumentObject();
+		ao.setIntValue(10);
+        int t = ao.getIntValue();
         assertEquals(t, 10);
     }
     
     @Test
-    public void AddArgLengthToAP(){
+    public void testObjectStringNull()
+	{
         ArgumentParser ap = new ArgumentParser();
-        ap.addArg("Length", 1);
-        int t = ap.getValue("Length");
-        assertEquals(1, t);
+		ArgumentParser.ArgumentObject ao = new ArgumentParser.ArgumentObject();
+        String s = ao.getStringValue();
+        assertEquals(null, s);
     }
     
     @Test
-    public void testMyObjectDescriptionIsNull(){
-        ArgumentParser ap = new ArgumentParser();
-	ArgumentObject ao = new ArgumentObject();
-	assertEquals(null, ao.getDescription());
-    }
-    
-    
+    public void testObjectDescriptionNull()
+	{
+		ArgumentParser ap = new ArgumentParser();
+		ArgumentParser.ArgumentObject ao = new ArgumentParser.ArgumentObject();
+		assertEquals(null, ao.getDescriptionValue());
+    } 
 }
