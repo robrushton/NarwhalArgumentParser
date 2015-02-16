a4ec0c235383d9003747c2197e5ecb279c65c76a
Kane Stapler <kanestapler@gmail.com>
2015-02-15 22:00:20 -0600

    Test

--- acceptance/VolumeCalculateKeywords.java
+++ acceptance/VolumeCalculateKeywords.java
@@ -1,17 +1,52 @@
-import edu.jsu.mcis.*
+import edu.jsu.mcis.*;
 
-public class VolumeClaculateKeywords{
-	public int getLength (){
-	ArgumentParser ap = new ArgumentParser;
-	ap.
-	}
-	
-	public int getWidth(){
+public class VolumeCalculateKeywords{
+    
+    ArgumentParser ap = new ArgumentParser();
+    
+    public void startProgramWithArguments(String[] args) {
+        ap.parse(args);
+    }
+    
+    public void initializeFirstTest() {
+        ap.addArguments("length", "Length of the object", "int");
+        ap.addArguments("width", "Width of the object", "int");
+        ap.addArguments("height", "Height of the object", "int");
+    }
+    
+    public void initializeSecondTest() {
+        ap.addArguments("pet", "It's a pet", "String");
+        ap.addArguments("number", "It's a number", "int");
+        ap.addArguments("rainy", "Is it raining", "boolean");
+        ap.addArguments("bathrooms", "how many bathrooms", "float");
+    }
+    
+    public int getLength (){
+        return ap.getIntValue("length");
+    }
 
-	}
+    public int getWidth(){
+        return ap.getIntValue("width");
+    }
 
-	public int Height(){
-		
-	}
+    public int getHeight(){
+        return ap.getIntValue("height");
+    }
+    
+    public String getPet(){
+        return ap.getStringValue("pet");
+    }
+    
+    public int getNumber(){
+        return ap.getIntValue("number");
+    }
+    
+    public boolean getRainy(){
+        return ap.getBooleanValue("rainy");
+    }
+    
+    public float getBathrooms(){
+        return ap.getFloatValue("bathrooms");
+    }
 
 }
