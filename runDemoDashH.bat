d0a94df26b7324b5c3483ebe7bc3cdf7b9b8fe31
Dwight Patton Christian III <dchristian@jsu.edu>
2015-03-02 00:08:47 -0600

    Create .bat demo for -h

---
+++ runDemoDashH.bat
@@ -0,0 +1,10 @@
+@echo off
+cd C:\Users\Trey\Desktop\CS310\narwhal-master\Demos
+echo "Running -h demo..."
+javac VolumeCalculator.java
+@echo on
+java VolumeCalculator -h
+@echo off
+echo.
+echo "-h demo complete!"
+pause
